cf2555e756ad594733f9ba451ba31784db5c6c4b
daniel <danielgonzalez252@gmail.com>
2023-03-28 00:05:23 -0400

    Lab Finished

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(argument = "roller-skate")
+{
+    return `This Saturday, I want to ${argument}!`;
+}
+
+function mondayWork(argument = 'go to the office' )
+{
+    return `This Monday, I will ${argument}.`;
+}
+
+function wrapAdjective(flair = "*")
+{
+    return function(adjective = "special")
+    {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
